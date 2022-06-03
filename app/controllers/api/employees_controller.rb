5984f61addcc2e5a5671fcfe8c47fcfd34fa83b2
James <jimibue@gmail.com>
2022-06-03 12:57:58 -0600

    demo

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -1,6 +1,35 @@
 class Api::EmployeesController < ApplicationController
 
+    # READ
+    # GET	/api/employees
     def index
         render json: Employee.all
+    end
+
+
+    # GET /api/employees/:id
+    def show
+        render json: Employee.find(params[:id])
+    end
+
+    #CREATE
+    # POST	/api/employees
+    def create
+        #TODO
+        employee = Employee.create(name: 'SOME NAME', postion:'some position')
+        render json employee
+    end
+
+    #UPDATE
+    #PUT	/api/employees/:id
+    def update
+        #TODO
+    end
+
+    #DESTROY
+    # DELETE /api/employees/:id
+    ## here destroy an employee
+    def destroy
+      render json: Employee.find(params[:id]).destroy
     end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [employees, setEmployees] = useState([])
+  // on mount grab employees (from our rails server !!!)
+  useEffect(()=>{
+    getEmployees()
+  },[])  
+
+  // function to grab employees from our rails api
+  const getEmployees= async() =>{
+    try{
+      let res = await axios.get('/api/employees')
+      setEmployees(res.data)
+    }catch(err){
+       alert('err occured getting employees')
+       console.log(err)
+    }
+  }
+
+  const deleteEmployee = async (id) => {
+    try{
+    // delete from db
+    let res = await axios.delete(`/api/employees/${id}`)
+    console.log(res)
+    // delete from my UI
+    let newEmployees = employees.filter(e=> e.id !== id)
+    setEmployees(newEmployees)
+    } catch(err){
+      alert('err occured deleting employees')
+      console.log(err)
+    }
+  }
+  const renderEmployees = ()=>{
+   return employees.map(e=>{
+     return(
+      <div className='emp' key={e.id}>
+        <h1>{e.name}</h1>
+        <p>{e.position}</p>
+        <button onClick={()=>{deleteEmployee(e.id)}}>delete</button>
+      </div>
+     )
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Employees</h1>
+        {renderEmployees()}
+        {/* <p>{JSON.stringify(employees)}</p> */}
     </div>
   );
 }
